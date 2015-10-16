7221d74481d1a462cb2ea2278dd4645477529a4f
Gaurav Sood <gsood07@gmail.com>
2015-10-15 21:27:51 -0400

    knn

--- R/knn.R
+++ R/knn.R
@@ -28,14 +28,12 @@ knn <- function (train, test, cl, k = 1)
     nte <- dim(test)[1]
     if (dim(test)[2] != p) 
         stop("Dims of test and train differ")
-    if (!is.loaded("knn")) 
-		dyn.load(paste(.libPaths()[1], "/dann/libs/dann.so", sep=""))
     if (is.factor(cl)) 
         cl1 <- as.numeric(cl)
     else cl1 <- cl
     z <- .Fortran("knn", as.integer(k), as.integer(ntr), as.integer(nte), 
         as.integer(p), as.double(train), as.integer(cl1), as.double(test), 
-        res = integer(nte), u = as.single(runif(nte)), d = double(nte), PACKAGE ="dann")
+        res = integer(nte), u = as.single(runif(nte)), d = double(nte))
     res <- z$res
     if (is.factor(cl)) {
         levels(res) <- levels(cl)
