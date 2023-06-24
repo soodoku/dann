25fb331f0b8306d2b002010918a3a96968ddef3d
soodoku <gsood07@gmail.com>
2023-06-24 12:33:33 -0700

    Update knn.R

--- R/knn.R
+++ R/knn.R
@@ -16,29 +16,37 @@
 #' knn(train, test, cl, k=2)
 #'}
 
-knn <- function (train, test, cl, k = 1) 
-{
-    train <- as.matrix(train)
-    test <- as.matrix(test)
-    p <- dim(train)[2]
-    ntr <- dim(train)[1]
-    if (length(cl) != ntr) 
-        stop("train and class have different lengths")
-    nte <- dim(test)[1]
-    if (dim(test)[2] != p) 
-        stop("Dims of test and train differ")
-    if (is.factor(cl)) {
-        cl1 <- as.numeric(cl)
-    } else {
-        cl1 <- cl
-    }
-    z <- .Fortran("knn", as.integer(k), as.integer(ntr), as.integer(nte), 
-        as.integer(p), as.double(train), as.integer(cl1), as.double(test), 
-        res = integer(nte), u = as.single(runif(nte)), d = double(nte), PACKAGE ="dann")
-    res <- z$res
-    if (is.factor(cl)) {
-        levels(res) <- levels(cl)
-        res <- as.factor(res)
-    }
-    res
+knn <- function(train, test, cl, k = 1) {
+  train <- as.matrix(train)
+  test <- as.matrix(test)
+  
+  ntr <- dim(train)[1]
+  nte <- dim(test)[1]
+  p <- dim(train)[2]
+  
+  if (length(cl) != ntr) 
+    stop("train and class have different lengths")
+  
+  if (dim(test)[2] != p) 
+    stop("Dims of test and train differ")
+  
+  if (is.factor(cl)) {
+    cl1 <- as.numeric(cl)
+  } else {
+    cl1 <- cl
+  }
+  
+  res <- integer(nte)
+  u <- as.single(runif(nte))
+  d <- double(nte)
+  
+  .Fortran("knn",
+           k, ntr, nte, p, train, cl1, test, res, u, d, PACKAGE = "dann")
+  
+  if (is.factor(cl)) {
+    levels(res) <- levels(cl)
+    res <- as.factor(res)
+  }
+  
+  return(res)
 }
