2f5977b1d5ded4cd8a1c4e0897e02516e74965df
soodoku <gsood07@gmail.com>
2023-06-24 12:36:51 -0700

    Update nndist.R

--- R/nndist.R
+++ R/nndist.R
@@ -22,25 +22,31 @@
 #' nndist()
 #'}
 
-nndist <-
-function (x, y, 
-    x0 = apply(x, 2, mean), k = length(y)/2, epsilon = 1, fullw = FALSE, 
-    scalar = FALSE, iter = 1, cv = 0) 
-{
+nndist <- function(x, y, x0 = apply(x, 2, mean), k = length(y)/2, epsilon = 1, fullw = FALSE, scalar = FALSE, iter = 1, cv = 0) {
+  if (is.matrix(x)) {
     storage.mode(x) <- "double"
+  } else {
+    stop("Input 'x' should be a matrix.")
+  }
+  
+  if (is.matrix(x0)) {
     storage.mode(x0) <- "double"
+  } else {
+    stop("Input 'x0' should be a matrix.")
+  }
+  
+  if (is.integer(y)) {
     storage.mode(y) <- "integer"
-    nclass <- length(table(y))
-    np <- dim(x)
-    p <- np[2]
-    n <- np[1]
-    junk <- .Fortran("nndist", as.integer(np[1]), as.integer(np[2]), 
-        as.integer(nclass), k = as.integer(k), x, x0, as.integer(cv), 
-        y, as.integer(iter), fullw, scalar, epsilon = as.double(epsilon), 
-        which = integer(n), dist = double(n), covw = matrix(double(p^2), 
-            p, p), covmin = as.double(1e-04), means = matrix(double(nclass * 
-            p), nclass, p), weight = double(n), values = double(p), 
-        vectors = double(p * p), double(p * p), double(n + 2 * 
-            p), PACKAGE ="dann")
-    return(junk)
+  } else {
+    stop("Input 'y' should be an integer vector.")
+  }
+  
+  nclass <- length(table(y))
+  np <- dim(x)
+  p <- np[2]
+  n <- np[1]
+  
+  junk <- .Fortran("nndist", as.integer(np[1]), as.integer(np[2]), as.integer(nclass), as.integer(k), x, x0, as.integer(cv), y, as.integer(iter), fullw, scalar, epsilon = as.double(epsilon), which = integer(n), dist = double(n), covw = matrix(double(p^2), p, p), covmin = as.double(1e-04), means = matrix(double(nclass * p), nclass, p), weight = double(n), values = double(p), vectors = double(p * p), double(p * p), double(n + 2 * p), PACKAGE = "dann")
+  
+  return(junk)
 }
