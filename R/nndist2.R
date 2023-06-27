0c19c6ea4016f486f79e4c88ebd3e66d9769ac19
soodoku <gsood07@gmail.com>
2023-06-26 21:39:53 -0700

    Update nndist2.R

--- R/nndist2.R
+++ R/nndist2.R
@@ -22,27 +22,23 @@
 #' @usage # dann()
 #'
 
-nndist2 <-
-function (x, y, 
-    x0 = apply(x, 2, mean), kmetric = length(y)/2, ktarget = 5, 
-    rate = 0.5, epsilon = 1, fullw = FALSE, scalar = FALSE, iter = 1, 
-    cv = 0) 
-{
-    storage.mode(x) <- "double"
-    storage.mode(x0) <- "double"
-    storage.mode(y) <- "integer"
-    nclass <- length(table(y))
-    np <- dim(x)
-    p <- np[2]
-    n <- np[1]
-    junk <- .Fortran("nndist2", as.integer(np[1]), as.integer(np[2]), 
-        as.integer(nclass), k = as.integer(c(kmetric, ktarget, 
-            iter)), rem = as.double(c(rate, epsilon, 1e-04)), 
-        x, x0, as.integer(cv), y, c(fullw, scalar), which = integer(n), 
-        dist = double(n), metric = matrix(double(p^2), p, p), 
-        covw = matrix(double(p^2), p, p), means = matrix(double(nclass * 
-            p), nclass, p), weight = double(n), values = double(p), 
-        vectors = double(p * p), double(p * p), double(n + 2 * 
-            p), x, ynew = y, kback = integer(2), PACKAGE ="dann")
-    return(junk)
+nndist2 <- function(x, y, x0 = colMeans(x), kmetric = length(y)/2, ktarget = 5, rate = 0.5, epsilon = 1, fullw = FALSE, scalar = FALSE, iter = 1, cv = 0) {
+  storage.mode(x) <- "double"
+  storage.mode(x0) <- "double"
+  storage.mode(y) <- "integer"
+  nclass <- length(table(y))
+  np <- dim(x)
+  p <- np[2]
+  n <- np[1]
+  junk <- .Fortran("nndist2", as.integer(n), as.integer(p), as.integer(nclass),
+                   k = as.integer(c(kmetric, ktarget, iter)),
+                   rem = as.double(c(rate, epsilon, 1e-04)),
+                   x, x0, as.integer(cv), y, c(fullw, scalar),
+                   which = integer(n), dist = double(n),
+                   metric = double(p^2), covw = double(p^2),
+                   means = double(nclass * p), weight = double(n),
+                   values = double(p), vectors = double(p * p),
+                   double(p * p), double(n + 2 * p), x,
+                   ynew = y, kback = integer(2), PACKAGE = "dann")
+  return(junk)
 }
